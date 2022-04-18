38342bf070461c32a763159eb4438ad58f63ccf7
ARIFULHAQUE1 <arifulhaquearif2326@gmail.com>
2022-04-18 19:23:35 +0600

    added NotFound content

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const NotFound = () => {
       return (
-            <div>
-                  <h4 className='text-danger'>Doctor was Sleeping...</h4>
+            <div className='mx-auto '>
+                  <h4 className='text-danger text-center'>Doctor was Sleeping...</h4>
+                <div className='mx-auto'>
+                <img className='w-100' src="https://img.freepik.com/free-vector/internet-network-warning-404-error-page-file-found-web-page-internet-error-page-issue-found-network-404-error-present-by-man-sleep-display_1150-55450.jpg?size=626&ext=jpg&ga=GA1.2.1652170954.1649070937" alt="" />
+                </div>
             </div>
       );
 };
